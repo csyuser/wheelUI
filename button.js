aee2b443d95f5aefda85e58d04cd0bae489fee52
csy <13540039093@163.com>
2020-09-01 23:22:39 +0800

    加入parcel，将button.js改为单文件组件

--- button.js
+++
@@ -1,6 +0,0 @@
-Vue.component('w-button',
-  {
-    template: `
-    <button class="w-button">你好</button>
-    `
-  })
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="w-button">你好</button>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss">
+.w-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  color: var(--color);
+  background: var(--button-bg);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  padding: 0 1em;
+  &:hover {
+    border: 1px solid var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
