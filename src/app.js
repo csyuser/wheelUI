271cceafb44032552229537c392d064c20ff7453
csyuser <13540039093@163.com>
2020-09-21 14:05:04 +0800

    用chai与mock进行单元测试

--- src/app.js
+++ src/app.js
@@ -14,3 +14,90 @@ new Vue({
         loading1:false
   }
   })
+
+//单元测试
+
+import chai from 'chai'
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData:{
+      icon:'settings'
+    }
+  })
+  vm.$mount('')
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+      loadings:true
+    }
+  })
+  vm.$mount('')
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+      iconPosition:'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+//mock 测试事件是否被触发
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+      iconPosition:'right'
+    }
+  })
+  vm.$mount()
+  vm.$on('click',function(){
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+  vm.$el.remove()
+  vm.$destroy()
+}
