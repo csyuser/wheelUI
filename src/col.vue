d84411bd26d448caf469ef4edd8378233f597b49
csyuser <13540039093@163.com>
2020-10-09 15:28:44 +0800

    完成gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col" :class="[`col-${span}`,`offset-${offset}`]">
-    <slot></slot>
+  <div class="col" :class="[span&&`col-${span}`,offset&&`offset-${offset}`]" :style="{paddingLeft:gutter/2 +'px',paddingRight:gutter/2 +'px'}">
+      <slot></slot>
   </div>
 </template>
 
@@ -14,19 +14,27 @@ export default {
     offset:{
       type:[Number,String]
     }
+  },
+  data(){
+    return{
+      gutter:0
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
 .col{
-  height: 50px;
   width: 50%;
   $class-prefix:col-;
   @for $n from 1 through 24{ &.#{$class-prefix}#{$n} {width: ($n / 24) *100%;} }
   $class-prefix:offset-;
   @for $n from 1 through 24{ &.#{$class-prefix}#{$n} {margin-left: ($n / 24) *100%;} }
-  &:nth-child(odd){background: #378df1;}
-  &:nth-child(even){background: #79b7ff;}
+  &:nth-child(odd)> .col-content{background: #378df1;}
+  &:nth-child(even)> .col-content{background: #79b7ff;}
+  .col-content{
+    height: 50px;
+    padding: 0 10px;
+  }
 }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2 +'px',marginRight:-gutter/2 +'px'}">
     <slot></slot>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name:'wheelRow',
+  props:{
+    gutter:{
+      type:[Number,String]
+    }
+  },
+  mounted(){
+    console.log(this.$children)
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter
+    })
+  }
+}
+</script>
 
 <style scoped lang="scss">
 .row{
