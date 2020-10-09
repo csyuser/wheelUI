a6945ab301524a9bf1f4818ae6568c7dad9041fa
csyuser <13540039093@163.com>
2020-10-09 14:29:02 +0800

    添加offset

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[`col-${span}`,`offset-${offset}`]">
     <slot></slot>
   </div>
 </template>
@@ -9,6 +9,9 @@ export default {
   name:'wheelCol',
   props:{
     span:{
+      type:[Number,String]
+    },
+    offset:{
       type:[Number,String]
     }
   }
@@ -21,6 +24,8 @@ export default {
   width: 50%;
   $class-prefix:col-;
   @for $n from 1 through 24{ &.#{$class-prefix}#{$n} {width: ($n / 24) *100%;} }
+  $class-prefix:offset-;
+  @for $n from 1 through 24{ &.#{$class-prefix}#{$n} {margin-left: ($n / 24) *100%;} }
   &:nth-child(odd){background: #378df1;}
   &:nth-child(even){background: #79b7ff;}
 }
