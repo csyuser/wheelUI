80a33d6e487975a16496f34a9f076619b29a5e5c
csyuser <13540039093@163.com>
2020-12-16 08:33:54 +0800

    新建popover

---
+++ src/popover.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="popover-wrap">{{item}}
+    <w-button @click="xxx">点我</w-button>
+    <w-button @click="yyy">点我2</w-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popover',
+  data() {
+    return {
+      item: {}
+    }
+  },
+  methods:{
+    xxx(){
+      console.log('点击了')
+      // this.item[0] = 'xxx'
+      // this.$set(this.item,1,'xxx')
+      this.item = {name:'xxx'}
+      console.log(this.item)
+    },
+    yyy(){
+      console.log('点击了yyy')
+      // this.item[0] = 'xxx'
+      this.$set(this.item,'age','yyy')
+      // this.item.age = 'yyy'
+      console.log(this.item)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
