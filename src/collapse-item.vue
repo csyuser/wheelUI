39dc38e4a131dba52e88e8de8aa70397845bc7ce
csyuser <13540039093@163.com>
2020-12-22 19:01:23 +0800

    collapse基本功能和样式完成

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="collapseItem-wrap">
-    <slot name="title"></slot>
-    <slot></slot>
+    <div class="title" @click="show=!show">{{ title }}</div>
+    <div class="content" v-if="show">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "WheelCollapseItem"
+  name: 'WheelCollapseItem',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data(){
+    return{
+      show:false
+    }
+  }
 }
 </script>
 
 <style scoped lang='scss'>
-
+$border-color: #d9d9d9;
+$radius: 3px;
+.collapseItem-wrap {
+  border: 1px solid $border-color;
+  border-bottom: none;
+  border-top: none;
+  &:first-child {
+    border-top: 1px solid $border-color;
+    border-top-left-radius: $radius;
+    border-top-right-radius: $radius;
+  }
+  &:last-child {
+    border-bottom: 1px solid $border-color;
+    border-bottom-left-radius: $radius;
+    border-bottom-right-radius: $radius;
+    > .content{
+      border-top: 1px solid $border-color;
+      border-bottom: none;
+    }
+    > .title{
+      border-bottom: none;
+    }
+    > .content {
+      border-bottom-left-radius: $radius;
+      border-bottom-right-radius: $radius;
+    }
+  }
+  > .title {
+    border-bottom: 1px solid $border-color;
+    background: #fafafa;
+    padding: 12px 16px;
+  }
+  > .content {
+    border-bottom: 1px solid $border-color;
+    padding: 16px;
+  }
+}
 </style>
--- src/collapse.vue
+++ src/collapse.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="collapse-wrap">
-  <slot></slot>
-</div>
+  <div class="collapse-wrap">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-name: "WheelCollapse"
+  name: 'WheelCollapse'
 }
 </script>
 
